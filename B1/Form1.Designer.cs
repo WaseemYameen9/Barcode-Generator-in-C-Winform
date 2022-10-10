7aae244b385d84590ae8d5e42b77153c42cd02da
waseem <wyameen152@gmail.com>
2022-10-10 18:10:28 +0500

    First Commit

---
+++ B1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace B1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.txtEncode = new System.Windows.Forms.TextBox();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.lblEncode = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnDecode);
+            this.panel1.Controls.Add(this.btnEncode);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.lblEncode);
+            this.panel1.Controls.Add(this.txtDecode);
+            this.panel1.Controls.Add(this.txtEncode);
+            this.panel1.Controls.Add(this.pic);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pic.Location = new System.Drawing.Point(0, 0);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(800, 163);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // txtEncode
+            // 
+            this.txtEncode.Location = new System.Drawing.Point(253, 228);
+            this.txtEncode.Name = "txtEncode";
+            this.txtEncode.Size = new System.Drawing.Size(283, 20);
+            this.txtEncode.TabIndex = 1;
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Location = new System.Drawing.Point(253, 269);
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(283, 20);
+            this.txtDecode.TabIndex = 2;
+            // 
+            // lblEncode
+            // 
+            this.lblEncode.AutoSize = true;
+            this.lblEncode.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEncode.Location = new System.Drawing.Point(177, 234);
+            this.lblEncode.Name = "lblEncode";
+            this.lblEncode.Size = new System.Drawing.Size(57, 17);
+            this.lblEncode.TabIndex = 3;
+            this.lblEncode.Text = "Encode";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Decode";
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.FlatAppearance.BorderSize = 0;
+            this.btnEncode.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncode.Location = new System.Drawing.Point(377, 316);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 27);
+            this.btnEncode.TabIndex = 5;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.FlatAppearance.BorderSize = 0;
+            this.btnDecode.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecode.Location = new System.Drawing.Point(461, 316);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 27);
+            this.btnDecode.TabIndex = 6;
+            this.btnDecode.Text = "QrCode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(418, 360);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 27);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblEncode;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.TextBox txtEncode;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ B1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace B1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            pic.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.Code128BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pic.Image = barcode.Draw(txtEncode.Text,200);
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            pic.SizeMode = PictureBoxSizeMode.AutoSize;
+            Zen.Barcode.CodeQrBarcodeDraw qrBarcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pic.Image = qrBarcode.Draw(txtEncode.Text, 200);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            pic.Image.Save(path + "\\" + DateTime.Now.Second.ToString() + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+    }
+}
